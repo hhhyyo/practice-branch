0355260c5b695f4ab1d2d7ffbffa100fa2c372cc
hhhyyo <gydnjsrh6923@korea.ac.kr>
2021-07-23 11:47:21 +0900

    feat: print hello if i ==3

--- hello.py
+++ hello.py
@@ -1,10 +1,12 @@
 animals=[
-    'rabbit',
-    'dog', //마지막 아이템 뒤에 ','를 붙이는 것을 Trailing comma 라고한다
-    'cat',
     'hamster',
+    'rabbit', //마지막 아이템 뒤에 ','를 붙이는 것을 Trailing comma 라고한다
+    'dog',
 ]
 
 
-for _ in range(1,10+1):
-    print('hello')
+for i in range(1,10+1):
+    if i%3==0:
+        print('hello')
+    else:
+        print(i)
